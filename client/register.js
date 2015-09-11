76cd2429de332c520a7bca6fa4b3fbe3578988ad
DeBraid <de.braid@gmail.com>
2015-09-11 18:11:16 -0400

    links to profiles added

--- client/register.js
+++ client/register.js
@@ -8,17 +8,31 @@ Template.login.events({
 Template.user.events({
   'click #signOut' : function() {
     Meteor.logout();
+  },
+});
+
+Template.register.events({
+  'click .search': function (e,t) {
+	var q = $(e.target).val();
+	console.log('searching w/ q', q);
+	search(q);
   }
 });
 
-Meteor.call('searchTweets', 'meteorJS', function (error, response) {
-	if (error) {
-		console.log('Error in searchTweets client side', error);
-	};
-	var statuses = response.result.statuses;
-	console.log(statuses);
-	Session.set('statuses', statuses);
-});
+
+search('#hamont');
+
+function search (query) {
+	Meteor.call('searchTweets', query, function (error, response) {
+		if (error) {
+			console.log('Error in searchTweets client side', error);
+		};
+		var statuses = response.result.statuses;
+		console.log(statuses);
+		Session.set('statuses', statuses);
+	});
+
+}
 
 Template.user.helpers({
 	searchedTweets: function () {
