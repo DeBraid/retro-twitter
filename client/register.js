a1de725cd9da6594943fa414798af26164c44247
DeBraid <de.braid@gmail.com>
2015-09-11 16:35:49 -0400

    fetching tweets with search, passing query in

--- client/register.js
+++ client/register.js
@@ -5,11 +5,23 @@ Template.login.events({
   }
 });
 
-Template.user.rendered = function () {
-  console.log(Meteor.user())
-};
 Template.user.events({
   'click #signOut' : function() {
     Meteor.logout();
   }
+});
+
+
+Template.user.helpers({
+	searchedTweets: function () {
+		var data;
+		Meteor.call('searchTweets', 'meteorJS', function (error, response, baz) {
+			if (error) {
+				console.log('Error in searchTweets client side', error);
+			};
+			console.log('response in .rendered', response.result);
+			data = response.result;
+		});
+		return data;
+	}
 });
--- server/methods.js
+++ server/methods.js
@@ -2,10 +2,27 @@
 /* Server Only Methods */
 /*****************************************************************************/
 Meteor.methods({
-  /*
-   * Example:
-   *
-   * '/app/items/insert': function (item) {
-   * }
-   */
+    'searchTweets': function getTweets(query) {
+        var TwitterApi = Meteor.npmRequire('twit');
+        var T = new TwitterApi({
+            consumer_key: Meteor.settings.local_twitter.consumerKey,
+            consumer_secret: Meteor.settings.local_twitter.secret,
+            access_token: Meteor.settings.local_twitter.access_token,
+            access_token_secret: Meteor.settings.local_twitter.access_token_secret,
+        });
+
+        var tweets = Async.runSync(function (done) {
+            T.get('search/tweets', {
+                    q: ''+ query +' since:2011-11-11',
+                    count: 10
+                },
+                function (err, data, response) {
+                    console.log('data in get', data)
+                    done(null, data);
+                }
+            );
+        });
+
+        return tweets;
+    }
 });
