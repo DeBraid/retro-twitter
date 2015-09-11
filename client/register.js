c588cbf42fa85fdece83a66254aa9e847ad46fed
DeBraid <de.braid@gmail.com>
2015-09-11 17:28:12 -0400

    cleaned up html

--- client/register.js
+++ client/register.js
@@ -16,16 +16,12 @@ Meteor.call('searchTweets', 'meteorJS', function (error, response) {
 		console.log('Error in searchTweets client side', error);
 	};
 	var statuses = response.result.statuses;
-	console.log('statuses', statuses);
-	Session.set('tweets', statuses);
-	// return response.result;
-	// return response.result.statuses;
+	console.log(statuses);
+	Session.set('statuses', statuses);
 });
 
 Template.user.helpers({
 	searchedTweets: function () {
-		return Session.get('tweets');
-		// console.log('return value of searchedTweets in user.helers', data);
-		// return data;
+		return Session.get('statuses');
 	}
 });
