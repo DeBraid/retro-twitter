dee705fa4ab848cdc6c1791e8f752c2982f1264e
DeBraid <de.braid@gmail.com>
2015-09-11 12:33:13 -0400

    added service auth meteor package, re deploy

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,13 +1,22 @@
 Meteor.startup(function() {
 
-  Accounts.loginServiceConfiguration.remove({
-    service : 'twitter'
-  });
+  // Accounts.loginServiceConfiguration.remove({
+  //   service : 'twitter'
+  // });
 
-  Accounts.loginServiceConfiguration.insert({
-    service     : 'twitter',
-    consumerKey : Meteor.settings.twitterAuth.consumerKey,
-    secret      : Meteor.settings.twitterAuth.secret
-  });
+  // Accounts.loginServiceConfiguration.insert({
+  //   service     : 'twitter',
+  //   consumerKey : Meteor.settings.twitterAuth.consumerKey,
+  //   secret      : Meteor.settings.twitterAuth.secret
+  // });
 
+	ServiceConfiguration.configurations.upsert(
+	  { service: "twitter" },
+	  {
+	    $set: {
+	      	consumerKey : Meteor.settings.twitterAuth.consumerKey,
+	    	secret      : Meteor.settings.twitterAuth.secret
+	    }
+	  }
+	);
 });
