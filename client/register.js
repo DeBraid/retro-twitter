9ee9037b1896b5fa00b67e9ec4243b2b4f41aa75
DeBraid <de.braid@gmail.com>
2015-09-11 20:34:45 -0400

    improved search responsiveness

--- client/register.js
+++ client/register.js
@@ -1,48 +1,35 @@
 Template.login.events({
-  'click #signIn' : function() {
-    console.log('sign in!');
-    Meteor.loginWithTwitter();
-  }
-});
-
-Template.user.events({
-  'click #signOut' : function() {
-    Meteor.logout();
-  },
+    'click #signIn': function () {
+        Meteor.loginWithTwitter();
+    }
 });
 
 Template.register.events({
-  'click #search': function (e,t) {
-	var q = $(e.target).val();
-	console.log('searching w/ q', q);
-	search(q);
-  },
-   'keypress #search': function (e, t) {
-    if (e.which === 13) {
-      var q = $(e.target).val();
-	console.log('searching w/ q', q);
-	search(q);
+    'click #search': function (e, t) {
+        search(e);
+    },
+    'keypress #search': function (e, t) {
+		var q = $(e.target).val();
+	    if ( (e.which === 13) || (q.length > 2) ) {
+	        search(e);
+	    }
     }
-  }
 });
 
+getSearchResultsFromApi('#hamont');
 
-search('#hamont');
-
-function search (query) {
-	Meteor.call('searchTweets', query, function (error, response) {
-		if (error) {
-			console.log('Error in searchTweets client side', error);
-		};
-		var statuses = response.result.statuses;
-		console.log(statuses);
-		Session.set('statuses', statuses);
-	});
-
+function search (e) {
+	var q = $(e.target).val();
+    getSearchResultsFromApi(q);
 }
 
-Template.user.helpers({
-	searchedTweets: function () {
-		return Session.get('statuses');
-	}
-});
+function getSearchResultsFromApi(query) {
+    Meteor.call('searchTweets', query, function (error, response) {
+        if (error) {
+            console.log('Error in searchTweets client side', error);
+        };
+        var statuses = response.result.statuses;
+        Session.set('statuses', statuses);
+    });
+}
+
---
+++ client/templates/user.js
@@ -0,0 +1,11 @@
+Template.user.events({
+    'click #signOut': function () {
+        Meteor.logout();
+    },
+});
+
+Template.user.helpers({
+    searchedTweets: function () {
+        return Session.get('statuses');
+    }
+});
