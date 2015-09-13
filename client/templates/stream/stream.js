edd28180b2e086b2beae1ba03a686dc8619f1271
DeBraid <de.braid@gmail.com>
2015-09-13 14:00:31 -0400

    refactoring stream and home controller

--- client/templates/stream/stream.js
+++ client/templates/stream/stream.js
@@ -1,7 +1,7 @@
 Template.user_stream.helpers({
 	user_stream: function () {
 		var data = Session.get('user_stream');
-		console.log('user_stream', data);
+		// console.log('user_stream', data);
 		return data;
 	}
 });
