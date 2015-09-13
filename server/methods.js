e83d6f3a680712d9ce47d83c37abf78b7fed333e
DeBraid <de.braid@gmail.com>
2015-09-13 10:11:27 -0400

    added links as separate entity

--- server/methods.js
+++ server/methods.js
@@ -12,7 +12,8 @@ var T = new TwitterApi({
 Meteor.methods({
     'searchTweets': function getTweets(query) {
         var tweets = Async.runSync(function (done) {
-            T.get('search/tweets', {
+            T.get('search/tweets', 
+                {
                     q: ''+ query +' since:2011-11-11',
                     count: 10
                 },
@@ -26,10 +27,9 @@ Meteor.methods({
     'getFollowerStream': function getFollowerStream(screenName) {
         var stream = [];
         var followers = Meteor.call('getFollowers', screenName );
-        // return followers;
         console.log('followers', followers);
         followers.result.forEach(function (follower) {
-            console.log('follower', follower);
+            // console.log('follower', follower);
 
             var timeline = Async.runSync(function (done) {
                 T.get('statuses/user_timeline', 
@@ -58,14 +58,12 @@ Meteor.methods({
     },
     'getFollowers': function getFollowers(screenName) {
         var followers = Async.runSync(function (done) {
-            //  get the list of user id's that follow screenName
             T.get('followers/ids', 
                 { screen_name: screenName,   count: 10 },
                 function (err, data, response) {
                     done(null, data.ids);
                 }
             );
-
         });
         return followers;
     }
