98cd4136de280b588b85a5fa12fc1416f35cab80
DeBraid <de.braid@gmail.com>
2015-09-13 15:42:49 -0400

    created login after hook

--- client/login.js
+++ client/login.js
@@ -1,6 +1,13 @@
 Template.twitter_auth.events({
     'click #signIn': function () {
-        Meteor.loginWithTwitter();
+        Meteor.loginWithTwitter(function loginWithTwitter () {
+        	var currentUser = Meteor.user();
+	        if (currentUser) {
+	            var name = currentUser.profile.name.replace(' ', '_');
+	            console.log('go to profile name', name);
+	            Router.go('stream', { screen_name: name } );
+	        };
+        });
     },
     'click #signOut': function () {
         Meteor.logout(function(err) {
